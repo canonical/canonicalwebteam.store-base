c8ae8ac6027c67e43b56ce704933475b3a15b63d
codeEmpress1 <halof.halima@gmail.com>
2023-02-06 19:08:25 +0300

    add basic config

--- canonicalwebteam/store_base/app.py
+++ canonicalwebteam/store_base/app.py
@@ -1,10 +1,8 @@
 """
 Extensible flask application base for all stores.
 
-Its gets response from the store_api and it extends each store blueprint to make a complete application
+It gets response from the store_api and it extends each store blueprint to make a complete application
 """
-import yamlloader
-# from canonicalwebteam.candid import FlaskBas
 from canonicalwebteam.flask_base.app import FlaskBase
 import canonicalwebteam.store_base.config as config
 from canonicalwebteam.store_base.utils.extensions import csrf
@@ -17,12 +15,11 @@ config would be passed in at store level
 
 a default config is supplied.
 """
-def create_app(app_name, testing=False, favicon_url=""):
+def create_app(app_name, store_bp=test_bp, testing=False):
 
-    app = FlaskBase(__name__,
-    app_name,
-    favicon_url=favicon_url,
-    )
+    app = FlaskBase(__name__, app_name)
+
+    app.register_blueprint(store_bp)
     app.config.from_object(config)
     app.testing = testing
 
--- canonicalwebteam/store_base/config.py
+++ canonicalwebteam/store_base/config.py
@@ -1,5 +1,3 @@
 import os
 
-APP_NAME = "storebase"
-SECRET_KEY = os.getenv("SECRET_KEY")
 ENVIRONMENT = os.getenv("ENVIRONMENT", "devel")
